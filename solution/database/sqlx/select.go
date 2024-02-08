a606dde19e1c4b5f0c078a00b5e5fb77ca5f3367
Nathan Castelein <nathan.castelein@gmail.com>
2024-02-08 10:45:30 +0100

    fix(tests): rewrite function signature

--- solution/database/sqlx/select.go
+++ solution/database/sqlx/select.go
@@ -37,21 +37,21 @@ func SelectAllPets() ([]shelter.Pet, error) {
 	return shelterPets, nil
 }
 
-func SelectOnePet(id int) (*shelter.Pet, error) {
+func SelectOnePet(id int) (shelter.Pet, error) {
 	query := `SELECT pet.id as id, pet.name as name, category.name as category FROM pet INNER JOIN category ON pet.category = category.id WHERE pet.id = $1;`
 
 	db, err := connect.SQLx()
 	if err != nil {
-		return nil, err
+		return shelter.Pet{}, err
 	}
 
 	pet := Pet{}
 	err = db.Get(&pet, query, id)
 	if err != nil {
-		return nil, err
+		return shelter.Pet{}, err
 	}
 
-	return &shelter.Pet{
+	return shelter.Pet{
 		Id:       pet.Id,
 		Name:     pet.Name,
 		Category: pet.Category,
