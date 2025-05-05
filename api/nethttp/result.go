73ab8fbb4dd9692f99ec5860e0137369ee46944a
Nathan Castelein <nathan.castelein@gmail.com>
2025-05-05 16:37:26 +0200

    Fix http exercise

--- api/nethttp/result.go
+++ api/nethttp/result.go
@@ -11,7 +11,7 @@ Using json.Marshal, encode the result in parameter
 If you encounter an error, write an status code 500, with the text "Internal Server Error".
 
 If you have no error, then:
-- write the status code 200
+- write the status code provided in the parameters
 - write a header "Content-Type" with the value "application/json"
 - write the encoded result
 */
